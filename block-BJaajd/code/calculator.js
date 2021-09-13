eebc429069c73b982cb242e713a627b229ad5324
priya pandey <priyarock746@gmail.com>
2021-09-13 15:42:39 +0530

    Assignment

--- block-BJaajd/code/calculator.js
+++ block-BJaajd/code/calculator.js
@@ -1,14 +1,41 @@
-function add() {}
+function add(a, b) {
+  if (typeof a ==='number' && typeof b === 'number'){
+    return a + b
+  }
+  return 'not a valid input'
+}
 
-function subtract() {}
+function subtract(a,b) {
+  return a - b
+}
 
-function sum() {}
+function sum(a, b) {
+  return a + b;
+}
+function multiply(a,b) {
+  return a * b;
+}
 
-function multiply() {}
+function power(base, exponent) {
+	var result = 1;
+	if(exponent == undefined)
+		exponent = 2;
+	for(var i=1; i<=exponent; i++) {
+		result = result * base;
+	}
+	return result;
+}
+// console.log(power(2,4));
 
-function power() {}
-
-function factorial() {}
+  function factorial(num) {
+    if (num === 0 || num === 1)
+      return 1;
+    for (var i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    return num;
+  }
+  factorial(5);
 
 module.exports = {
   add,
